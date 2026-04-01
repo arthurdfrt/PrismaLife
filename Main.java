0a4e110cb2bb2d0966cf661e7459fc58f3225d38
Arthur Freitas <arthurengenhariadacomp@gmail.com>
2026-04-01 18:29:41 -0300

    chore: add .gitignore to ignore .class files

--- Main.java
+++ Main.java
@@ -1,22 +1,14 @@
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         UserRegistry registry = new UserRegistry();
         User u1 = new User(1, "arthur", "arthur@email.com");
         registry.registerUser(u1);
 
         User loggedUser = registry.findByEmail("arthur@email.com");
-
-        TaskList mylist = new TaskList();
-
-        if (loggedUser != null) {
-
-            loggedUser.getMyTasks().createTask(new Task(1, "Jantar", 25, "medium"));
-            loggedUser.getMyTasks().createTask(new Task(2, "Finalizar a TodoList", 26, "High"));
-            loggedUser.getMyTasks().createTask(new Task(3, "Beber água de coco", 25, "Low"));
-
-            loggedUser.getMyTasks().showTasks();
-        }
     }
 }
--- User.java
+++ User.java
@@ -1,26 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private int id;
     private String name;
     private String email;
     public String password;
-    private TaskList myTasks;
-    
+    private List<LifeArea> silos;
+
+    private static int MAX_SILOS = 6;
+
     //////////////////////////////////////
-    
-    public User(int id, String name, String email){
+
+    public User(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.myTasks = new TaskList();
+        this.silos = new ArrayList<>(0);
+        this.silos.add(new LifeArea("Geral"));
     }
 
     //////////////////////////////////////
 
-    public TaskList getMyTasks(){
-        return myTasks;
+    public boolean createSilo(String lifeAreaName) {
+        if (this.silos.size() >= MAX_SILOS) {
+            System.out.println("Número máximo de áreas atingido");
+            return false;
+        }
+
+        this.silos.add(new LifeArea(lifeAreaName));
+        System.out.println("Área criada");
+        return true;
     }
 
-    public String getEmail(){
+    public List<LifeArea> getSilos() {
+        return silos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
         return email;
     }
 }
